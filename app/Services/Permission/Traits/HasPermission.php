3e0cff090e6b1113d800f1950b963d9c7965fa03
Amin <amin.esmkhani75@gmail.com>
2021-08-15 21:09:56 +0430

    Personal Access User In DBgit add .

---
+++ app/Services/Permission/Traits/HasPermission.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace App\Services\Permission\Traits;
+
+
+use App\Models\Permission;
+
+trait HasPermission
+{
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class);
+    }
+
+    public function givePermissionsTo(... $permissions)
+    {
+        $permissions = $this->getAllPermissions($permissions);
+
+        if ($permissions->isEmpty()) return $this;
+
+        $this->permissions()->syncWithoutDetaching($permissions);
+
+        return $this;
+    }
+
+    protected function getAllPermissions(array $permissions)
+    {
+        return Permission::whereIn('name', array_flatten($permissions))->get();
+    }
+
+}
