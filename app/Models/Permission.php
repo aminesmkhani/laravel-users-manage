fcf4ef398bd7e5b23a6c73c45c3c4424f75f9090
Amin Esmkhani <amin.esmkhani75@gmail.com>
2021-09-27 18:25:14 +0330

    make Relation Role & Permission

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Permission extends Model
 {
     use HasFactory;
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
 }
