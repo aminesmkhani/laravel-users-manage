689accd452ba3e2ea4c9842a4812b106851c6034
Amin Esmkhani <amin.esmkhani75@gmail.com>
2021-09-27 18:15:27 +0330

    use HasPermission Trait in Role Model!

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,11 +2,12 @@
 
 namespace App\Models;
 
+use App\Services\Permission\Traits\HasPermission;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    use HasFactory;
+    use HasFactory, HasPermission;
 
 }
