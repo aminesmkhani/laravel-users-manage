f0539a69edd8d5e8fbdf70e666b6e13696a30d66
Amin Esmkhani <amin.esmkhani75@gmail.com>
2021-09-30 19:37:25 +0330

    Pass Role Date to the View!

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Role;
 use Illuminate\Http\Request;
 
 class RoleController extends Controller
 {
     public function index()
     {
-        return view('roles.list');
+        $roles = Role::all();
+        return view('roles.list',compact('roles'));
     }
 }
