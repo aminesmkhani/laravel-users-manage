ccb531c1ba638e52b63bc804fb82c1941bfcc322
Amin Esmkhani <amin.esmkhani75@gmail.com>
2021-10-02 20:15:05 +0330

    Fix Edit Ui Role Edit!

--- resources/views/roles/edit.blade.php
+++ resources/views/roles/edit.blade.php
@@ -1,34 +1,45 @@
 @extends('layouts.layouts')
-@section('title','ویرایش کاربر')
+@section('title','لیست کاربران')
 @section('content')
-<div class="content-body">
-    <!-- Basic Checkbox start -->
-    <section class="basic-checkbox">
-        <form method="post" action="{{route('users.update', $user->id)}}">
-            @csrf
+    <div class="content-body">
+        <form class="form" method="POST" action="#">
+        <section id="multiple-column-form">
             <div class="row">
                 <div class="col-12">
                     <div class="card">
                         <div class="card-header">
-                            <h4 class="card-title">افزودن نقش به کاربر</h4>
+                            <h4 class="card-title">افزودن نقش</h4>
                         </div>
                         <div class="card-body">
-                            <div class="demo-inline-spacing">
-                                @forelse($roles as $role)
-                                <div class="form-check form-check-inline">
-                                    <input class="form-check-input" type="checkbox" {{$user->roles->contains($role) ?    'checked' : ''}} name="roles[]" value="{{$role->name}}" id="{{'role' . $role->id}}">
-                                    <label class="form-check-label" for="{{'role' . $role->id}}">{{$role->fa_name}}</label>
+                                @csrf
+                                <div class="row">
+                                    <div class="col-md-3 col-4">
+                                        <div class="form-group">
+                                            <label for="roleName">نام نقش</label>
+                                            <input type="text" id="roleName" value="{{$role->name}}" class="form-control" placeholder="نام نقش" name="name">
+                                            @if($errors->has('name'))
+                                                <small class="form-text text-danger">{{$errors->first('name')}}</small>
+                                            @endif
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3 col-4">
+                                        <div class="form-group">
+                                            <label for="roleNameFa">نام فارسی نقش</label>
+                                            <input type="text" id="roleNameFa" value="{{$role->fa_name}}" class="form-control" placeholder="نام فارسی نقش" name="fa_name">
+                                            @if($errors->has('fa_name'))
+                                                <small class="form-text text-danger">{{$errors->first('fa_name')}}</small>
+                                            @endif
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <button type="submit" class="btn btn-primary mr-1 waves-effect waves-float waves-light" style="margin-top: 20px">ثبت</button>
+                                    </div>
                                 </div>
-                                @empty
-                                    <p>
-                                        هیچ نقشی موجود نیست
-                                    </p>
-                                @endforelse
-                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+        </section>
             <div class="row">
                 <div class="col-12">
                     <div class="card">
@@ -39,7 +50,7 @@
                             <div class="demo-inline-spacing">
                                 @forelse($permissions as $permission)
                                     <div class="form-check form-check-inline">
-                                        <input class="form-check-input" type="checkbox" name="permissions[]" {{$user->permissions->contains($permission) ?    'checked' : ''}} value="{{$permission->name}}" id="{{'permission' . $permission->id}}">
+                                        <input class="form-check-input" type="checkbox" name="permissions[]" {{$role->permissions->contains($permission) ?    'checked' : ''}} value="{{$permission->name}}" id="{{'permission' . $permission->id}}">
                                         <label class="form-check-label" for="{{'permission' . $permission->id}}">{{$permission->fa_name}}</label>
                                     </div>
                                 @empty
@@ -52,10 +63,6 @@
                     </div>
                 </div>
             </div>
-            <button type="submit" class="btn btn-success waves-effect waves-float waves-light">افزودن</button>
         </form>
-
-    </section>
-    <!-- Basic Checkbox end -->
-</div>
+    </div>
 @endsection
