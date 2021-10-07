293d8fcc6abef8ae0e739d3a766b7bcef596fd68
Amin Esmkhani <amin.esmkhani75@gmail.com>
2021-10-07 17:39:13 +0330

    Define Middleware in Laravel Kernel!

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -16,7 +16,7 @@ class RoleMiddleware
      */
     public function handle(Request $request, Closure $next, $role)
     {
-        if (!$request->user() || $request->user()->hasRole($role)){
+        if (!$request->user() || !$request->user()->hasRole($role)){
             abort(404);
         }
         return $next($request);
